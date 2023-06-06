2263eee7cb261abb094251d77e4ee05d2153f2cc
kit0-0 <kt1767@gmail.com>
2023-06-06 13:45:35 +0530

    Add displaybook functionality

--- script.js
+++ script.js
@@ -10,6 +10,36 @@ class BookManager {
     this.addEventListeners();
   }
 
+  displayBooks() {
+    this.booksDiv.innerHTML = '';
+
+    if (this.books.length === 0) {
+      this.booksDiv.textContent = 'No books in the collection.';
+      return;
+    }
+
+    const removeBook = (index) => {
+      this.books.splice(index, 1);
+      localStorage.setItem('books', JSON.stringify(this.books));
+      this.displayBooks();
+    };
+
+    this.books.forEach((book, index) => {
+      this.booksDiv.innerHTML += `<div class="book">
+          <span class="book-title">${book.title}</span>
+          <span class="book-author">by ${book.author}</span>
+          <button class="remove-button" type="button" data-index="${index}">Remove</button>
+        </div>`;
+    });
+
+    const removeButtons = document.getElementsByClassName('remove-button');
+    Array.from(removeButtons).forEach((button, index) => {
+      button.addEventListener('click', () => {
+        removeBook(index);
+      });
+    });
+  }
+
   addBook(title, author) {
     const book = {
       title,
